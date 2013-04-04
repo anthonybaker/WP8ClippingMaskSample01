619475fa587a12171aec97d8ddd12451be78255c
Anthony Baker <abakerp@gmail.com>
2013-04-04 21:37:53 +0100

    Initial commit.
    Sample project demonstrating clipping masks in Windows Phone 8. The
    sample implements the clipping mask examples using XAML code only.

---
+++ ClippingMaskSample_01/ClippingMaskSample_01/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using ClippingMaskSample_01.Resources;
+
+namespace ClippingMaskSample_01
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,10 @@
+Author:		Anthony Baker
+Name:		ClippingMaskSample_01
+Date Created:	04/04/2013
+Platform:	Windows Phone 8
+
+Description:
+
+Simple project demonstrating how to create clipping masks for UI elements
+in XAML code for Windows Phone 8.
+		
